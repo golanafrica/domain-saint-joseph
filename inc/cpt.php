8771e9ceffde42e9dafaa34d5c85f12419b91a74
golanafrica <ifbburkina@outlook.fr>
2026-08-21 14:28:01 +0000

    fix: cpt.php - tous CPT dans la fonction init + capability_type

--- inc/cpt.php
+++ inc/cpt.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Custom Post Types &#8212; Formations, H&#233;bergements, Galerie
+ * Custom Post Types &#8212; Formations, H&#233;bergements, Galerie, Menu, T&#233;moignages
  * Domaine Saint Joseph
  */
 
 function dsj_register_cpts() {
     
-    // &#127891; Formations &#127891;
+    // &#127891; FORMATIONS &#127891;
     register_post_type( 'formation', [
         'labels' => [
             'name' => __( 'Formations', 'domaine-saint-joseph' ),
@@ -24,9 +24,10 @@ function dsj_register_cpts() {
         'menu_icon' => 'dashicons-welcome-learn-more',
         'show_in_rest' => false,
         'menu_position' => 5,
+        'capability_type' => 'post',
     ]);
 
-    // &#127968; H&#233;bergements &#127968;
+    // &#127968; H&#201;BERGEMENTS &#127968;
     register_post_type( 'hebergement', [
         'labels' => [
             'name' => __( 'H&#233;bergements', 'domaine-saint-joseph' ),
@@ -44,9 +45,10 @@ function dsj_register_cpts() {
         'menu_icon' => 'dashicons-building',
         'show_in_rest' => false,
         'menu_position' => 6,
+        'capability_type' => 'post',
     ]);
 
-    // &#128248; Galerie &#128248;
+    // &#128248; GALERIE &#128248;
     register_post_type( 'galerie', [
         'labels' => [
             'name' => __( 'Galerie', 'domaine-saint-joseph' ),
@@ -64,9 +66,10 @@ function dsj_register_cpts() {
         'menu_icon' => 'dashicons-format-gallery',
         'show_in_rest' => false,
         'menu_position' => 7,
+        'capability_type' => 'post',
     ]);
 
-    // &#128193; Taxonomie Cat&#233;gories Galerie &#128193;
+    // &#128193; TAXONOMIE CAT&#201;GORIES GALERIE &#128193;
     register_taxonomy( 'categorie_galerie', 'galerie', [
         'labels' => [
             'name' => __( 'Cat&#233;gories Galerie', 'domaine-saint-joseph' ),
@@ -80,60 +83,61 @@ function dsj_register_cpts() {
         'show_in_rest' => false,
         'rewrite' => [ 'slug' => 'categorie-galerie' ],
     ]);
+
+    // &#127869; RESTAURANT / MENU &#127869;
+    register_post_type( 'menu', [
+        'labels' => [
+            'name' => __( 'Menus Restaurant', 'domaine-saint-joseph' ),
+            'singular_name' => __( 'Plat', 'domaine-saint-joseph' ),
+            'add_new' => __( 'Ajouter un plat', 'domaine-saint-joseph' ),
+            'add_new_item' => __( 'Nouveau plat', 'domaine-saint-joseph' ),
+            'edit_item' => __( 'Modifier le plat', 'domaine-saint-joseph' ),
+            'view_item' => __( 'Voir le plat', 'domaine-saint-joseph' ),
+            'all_items' => __( 'Tous les plats', 'domaine-saint-joseph' ),
+        ],
+        'public' => true,
+        'has_archive' => true,
+        'rewrite' => [ 'slug' => 'menu' ],
+        'supports' => [ 'title', 'editor', 'thumbnail', 'excerpt' ],
+        'menu_icon' => 'dashicons-food',
+        'show_in_rest' => false,
+        'menu_position' => 8,
+        'capability_type' => 'post',
+    ]);
+
+    // &#128193; TAXONOMIE CAT&#201;GORIES DE PLATS &#128193;
+    register_taxonomy( 'categorie_menu', 'menu', [
+        'labels' => [
+            'name' => __( 'Cat&#233;gories de plats', 'domaine-saint-joseph' ),
+            'singular_name' => __( 'Cat&#233;gorie', 'domaine-saint-joseph' ),
+            'add_new_item' => __( 'Ajouter une cat&#233;gorie', 'domaine-saint-joseph' ),
+            'edit_item' => __( 'Modifier la cat&#233;gorie', 'domaine-saint-joseph' ),
+            'all_items' => __( 'Toutes les cat&#233;gories', 'domaine-saint-joseph' ),
+        ],
+        'public' => true,
+        'hierarchical' => true,
+        'show_in_rest' => false,
+        'rewrite' => [ 'slug' => 'categorie-menu' ],
+    ]);
+
+    // &#128172; T&#201;MOIGNAGES &#128172;
+    register_post_type( 'temoignage', [
+        'labels' => [
+            'name' => __( 'T&#233;moignages', 'domaine-saint-joseph' ),
+            'singular_name' => __( 'T&#233;moignage', 'domaine-saint-joseph' ),
+            'add_new' => __( 'Ajouter un t&#233;moignage', 'domaine-saint-joseph' ),
+            'add_new_item' => __( 'Nouveau t&#233;moignage', 'domaine-saint-joseph' ),
+            'edit_item' => __( 'Modifier le t&#233;moignage', 'domaine-saint-joseph' ),
+            'view_item' => __( 'Voir le t&#233;moignage', 'domaine-saint-joseph' ),
+            'all_items' => __( 'Tous les t&#233;moignages', 'domaine-saint-joseph' ),
+        ],
+        'public' => true,
+        'has_archive' => false,
+        'supports' => [ 'title', 'editor' ],
+        'menu_icon' => 'dashicons-testimonial',
+        'show_in_rest' => false,
+        'menu_position' => 9,
+        'capability_type' => 'post',
+    ]);
 }
 add_action( 'init', 'dsj_register_cpts' );
-
-
-// &#127869; Restaurant / Menu &#127869;
-register_post_type( 'menu', [
-    'labels' => [
-        'name' => __( 'Menus Restaurant', 'domaine-saint-joseph' ),
-        'singular_name' => __( 'Plat', 'domaine-saint-joseph' ),
-        'add_new' => __( 'Ajouter un plat', 'domaine-saint-joseph' ),
-        'add_new_item' => __( 'Nouveau plat', 'domaine-saint-joseph' ),
-        'edit_item' => __( 'Modifier le plat', 'domaine-saint-joseph' ),
-        'view_item' => __( 'Voir le plat', 'domaine-saint-joseph' ),
-        'all_items' => __( 'Tous les plats', 'domaine-saint-joseph' ),
-    ],
-    'public' => true,
-    'has_archive' => true,
-    'rewrite' => [ 'slug' => 'menu' ],
-    'supports' => [ 'title', 'editor', 'thumbnail', 'excerpt' ],
-    'menu_icon' => 'dashicons-food',
-    'show_in_rest' => false,
-    'menu_position' => 8,
-]);
-
-// &#128193; Taxonomie Cat&#233;gories de plats &#128193;
-register_taxonomy( 'categorie_menu', 'menu', [
-    'labels' => [
-        'name' => __( 'Cat&#233;gories de plats', 'domaine-saint-joseph' ),
-        'singular_name' => __( 'Cat&#233;gorie', 'domaine-saint-joseph' ),
-        'add_new_item' => __( 'Ajouter une cat&#233;gorie', 'domaine-saint-joseph' ),
-        'edit_item' => __( 'Modifier la cat&#233;gorie', 'domaine-saint-joseph' ),
-        'all_items' => __( 'Toutes les cat&#233;gories', 'domaine-saint-joseph' ),
-    ],
-    'public' => true,
-    'hierarchical' => true,
-    'show_in_rest' => false,
-    'rewrite' => [ 'slug' => 'categorie-menu' ],
-]);
-
-// &#128172; T&#233;moignages &#128172;
-register_post_type( 'temoignage', [
-    'labels' => [
-        'name' => __( 'T&#233;moignages', 'domaine-saint-joseph' ),
-        'singular_name' => __( 'T&#233;moignage', 'domaine-saint-joseph' ),
-        'add_new' => __( 'Ajouter un t&#233;moignage', 'domaine-saint-joseph' ),
-        'add_new_item' => __( 'Nouveau t&#233;moignage', 'domaine-saint-joseph' ),
-        'edit_item' => __( 'Modifier le t&#233;moignage', 'domaine-saint-joseph' ),
-        'view_item' => __( 'Voir le t&#233;moignage', 'domaine-saint-joseph' ),
-        'all_items' => __( 'Tous les t&#233;moignages', 'domaine-saint-joseph' ),
-    ],
-    'public' => true,
-    'has_archive' => false,
-    'supports' => [ 'title', 'editor' ],
-    'menu_icon' => 'dashicons-testimonial',
-    'show_in_rest' => false,
-    'menu_position' => 9,
-]);
